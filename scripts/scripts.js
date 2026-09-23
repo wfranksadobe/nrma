000f840934d59dc6ac599c6ac0ef21b4c9e025df
William Franks <wfranks@adobe.com>
2026-09-23 18:00:37 +1000

    added alloy.js and edited scripts.js to inject AEP library for target personalisation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -137,6 +137,121 @@ export function decorateMain(main) {
   a11yLinks(main);
 }
 
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Added this as per https://www.aem.live/developer/target-integration documentation to enable target integration.
+ * @param {*} path 
+ * @param {*} config 
+ * @returns 
+ */
+function initWebSDK(path, config) {
+  // Preparing the alloy queue
+  if (!window.alloy) {
+    // eslint-disable-next-line no-underscore-dangle
+    (window.__alloyNS ||= []).push('alloy');
+    window.alloy = (...args) => new Promise((resolve, reject) => {
+      window.setTimeout(() => {
+        window.alloy.q.push([resolve, reject, args]);
+      });
+    });
+    window.alloy.q = [];
+  }
+  // Loading and configuring the websdk
+  return new Promise((resolve) => {
+    import(path)
+      .then(() => window.alloy('configure', config))
+      .then(resolve);
+  });
+}
+
+function onDecoratedElement(fn) {
+  // Apply propositions to all already decorated blocks/sections
+  if (document.querySelector('[data-block-status="loaded"],[data-section-status="loaded"]')) {
+    fn();
+  }
+
+  const observer = new MutationObserver((mutations) => {
+    if (mutations.some((m) => m.target.tagName === 'BODY'
+      || m.target.dataset.sectionStatus === 'loaded'
+      || m.target.dataset.blockStatus === 'loaded')) {
+      fn();
+    }
+  });
+  // Watch sections and blocks being decorated async
+  observer.observe(document.querySelector('main'), {
+    subtree: true,
+    attributes: true,
+    attributeFilter: ['data-block-status', 'data-section-status'],
+  });
+  // Watch anything else added to the body
+  observer.observe(document.querySelector('body'), { childList: true });
+}
+
+function toCssSelector(selector) {
+  return selector.replace(/(\.\S+)?:eq\((\d+)\)/g, (_, clss, i) => `:nth-child(${Number(i) + 1}${clss ? ` of ${clss})` : ''}`);
+}
+
+async function getElementForProposition(proposition) {
+  const selector = proposition.data.prehidingSelector
+    || toCssSelector(proposition.data.selector);
+  return document.querySelector(selector);
+}
+
+async function getAndApplyRenderDecisions() {
+  // Get the decisions, but don't render them automatically
+  // so we can hook up into the AEM EDS page load sequence
+  const response = await window.alloy('sendEvent', { renderDecisions: false });
+  const { propositions } = response;
+  onDecoratedElement(async () => {
+    await window.alloy('applyPropositions', { propositions });
+    // keep track of propositions that were applied
+    propositions.forEach((p) => {
+      p.items = p.items.filter((i) => i.schema !== 'https://ns.adobe.com/personalization/dom-action' || !getElementForProposition(i));
+    });
+  });
+
+  // Reporting is deferred to avoid long tasks
+  window.setTimeout(() => {
+    // Report shown decisions
+    window.alloy('sendEvent', {
+      xdm: {
+        eventType: 'decisioning.propositionDisplay',
+        _experience: {
+          decisioning: { propositions },
+        },
+      },
+    });
+  });
+}
+
+let alloyLoadedPromise = initWebSDK('./alloy.js', {
+    datastreamId: '43e2ebdd-40fc-405e-8cdf-3ceeb90e7725',
+    orgId: '3836FBBE59C906A30A495E06@AdobeOrg',
+  });;
+if (getMetadata('target')) {
+  alloyLoadedPromise.then(() => getAndApplyRenderDecisions());
+}
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Loads everything needed to get to LCP.
  * @param {Element} doc The container element
@@ -148,11 +263,32 @@ async function loadEager(doc) {
     doc.body.dataset.breadcrumbs = true;
   }
   const main = doc.querySelector('main');
+
+
+  /**
   if (main) {
     decorateMain(main);
     document.body.classList.add('appear');
     await loadSection(main.querySelector('.section'), waitForFirstImage);
   }
+  */
+  if (main) {
+    decorateMain(main);
+    document.body.classList.add('appear');
+    // wait for alloy to finish loading
+    await alloyLoadedPromise;
+    // break up possible long tasks before showing the LCP block to reduce TBT
+    await new Promise((res) => {
+      window.setTimeout(async () => {
+        // For newer AEM boilerplate, use this
+        await loadSection(main.querySelector('.section'), waitForFirstImage)
+        // For older AEM boilerplate versions, use this instead
+        // await waitForLCP(LCP_BLOCKS);
+        res();
+      }, 0);
+    });
+  }
+
 
   try {
     /* if desktop (proxy for fast connection) or fonts already loaded, load fonts.css */
